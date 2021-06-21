abd7402e9a76e84718cd469ffc2c88c3647867db
Nur Ali Sholikhin <ali.sumberejo1@gmail.com>
2021-06-21 10:52:02 +0700

    add connection & admin

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,24 +3,55 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Welcome extends CI_Controller
 {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
-		$this->load->view('home');
+		$this->load->view('index');
+	}
+
+	public function admin()
+	{
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email', [
+			'required' => 'Harap isi bidang email!',
+			'valid_email' => 'Email tidak valid!',
+		]);
+		$this->form_validation->set_rules('password', 'Password', 'trim|required', [
+			'required' => 'Harap isi Password!',
+		]);
+		if ($this->form_validation->run() == false) {
+			$this->load->view('admin/login');
+		} else {
+			//validasi sukses
+			$this->adminlogin();
+		}
+	}
+
+	private function adminlogin()
+	{
+		$email = $this->input->post('email');
+		$password = $this->input->post('password');
+
+		$user = $this->db->get_where('admin', ['email' => $email])->row_array();
+
+		if ($user) {
+			//cek password
+			if (password_verify($password, $user['password'])) {
+				$data = [
+
+					'email' => $user['email'],
+					'nama' => $user['username'],
+
+				];
+				$this->session->set_userdata($data);
+				redirect(base_url('admin'));
+			} else {
+
+				$this->session->set_flashdata('fail-pass', 'Gagal!');
+				redirect(base_url('welcome/admin'));
+			}
+		} else {
+
+			$this->session->set_flashdata('fail-login', 'Gagal!');
+			redirect(base_url('welcome/admin'));
+		}
 	}
 }
--- application/views/index.php
+++ application/views/index.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 	<meta name="description" content="" />
 	<meta name="author" content="" />
-	<title>PBI - RW 07d</title>
+	<title>PBI - RW 07</title>
 	<!-- Favicon-->
 	<link rel="icon" type="image/x-icon" href="<?php echo base_url() ?>assets/favicon.ico" />
 	<!-- Bootstrap Icons-->
@@ -24,7 +24,7 @@
 	<!-- Navigation-->
 	<nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
 		<div class="container px-4 px-lg-5">
-			<a class="navbar-brand" href="#page-top">PBI RW-07</a>
+			<a class="navbar-brand" href="<?php echo base_url() ?>">PBI RW-07</a>
 			<button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
 			<div class="collapse navbar-collapse" id="navbarResponsive">
 				<ul class="navbar-nav ms-auto my-2 my-lg-0">
